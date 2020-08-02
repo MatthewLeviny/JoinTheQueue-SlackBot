cf2ea3a537df7f00186ee9068bd09d6c28a9624d
Matthew Leviny <matthew.leviny@carsales.com.au>
2020-08-02 21:05:35 +1000

    Tests for leave queue

--- JoinTheQueue.Test/Services/QueueServicesTests.cs
+++ JoinTheQueue.Test/Services/QueueServicesTests.cs
@@ -14,7 +14,7 @@ namespace JoinTheQueue.Test.Services
     public class QueueServicesTests : MoqAutoMocker<QueueServices>
     {
         [Theory]
-        [MemberData(nameof(RequestAndResponse))]
+        [MemberData(nameof(RequestAndResponseJoin))]
         public async Task JoinQueue(SlashRequest request, SlackResponseDto expectedResponse,
             QueueDto databaseResponseGet, QueueDto databaseResponseUpdate)
         {
@@ -42,7 +42,36 @@ namespace JoinTheQueue.Test.Services
             response.text.Should().Be(expectedResponse.text);
         }
 
-        public static IEnumerable<object[]> RequestAndResponse()
+        [Theory]
+        [MemberData(nameof(RequestAndResponseLeave))]
+        public async Task LeaveQueue(SlashRequest request, SlackResponseDto expectedResponse,
+            QueueDto databaseResponseGet, QueueDto databaseResponseUpdate)
+        {
+            //arrange
+            Mock.Get(Get<IQueueDatabase>()).Setup(database => database.GetQueue(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(Task.FromResult(databaseResponseGet == null
+                    ? null
+                    : new QueueDto
+                    {
+                        Queue = databaseResponseGet.Queue,
+                        ChannelId = databaseResponseGet.ChannelId
+                    }));
+
+            Mock.Get(Get<IQueueDatabase>()).Setup(database => database.UpdateQueue(It.IsAny<QueueDto>()))
+                .Returns(Task.FromResult(new QueueDto
+                {
+                    Queue = databaseResponseUpdate?.Queue,
+                    ChannelId = databaseResponseUpdate?.ChannelId
+                }));
+
+            //act
+            var response = await ClassUnderTest.LeaveQueue(request);
+            //assert
+            response.response_type.Should().Be(expectedResponse.response_type);
+            response.text.Should().Be(expectedResponse.text);
+        }
+
+        public static IEnumerable<object[]> RequestAndResponseJoin()
         {
             //No Queue
             yield return new object[]
@@ -87,6 +116,56 @@ namespace JoinTheQueue.Test.Services
                 {
                     ChannelId = "123",
                     Queue = new Queue<string>(new List<string> {"123"})
+                }
+            };
+        }
+
+        public static IEnumerable<object[]> RequestAndResponseLeave()
+        {
+            //No Queue
+            yield return new object[]
+            {
+                new SlashRequest
+                {
+                    Channel_Id = "123",
+                    Enterprise_Id = "123"
+                },
+                new SlackResponseDto
+                {
+                    text = "Queue Does not exist",
+                    response_type = BasicResponseTypes.ephemeral
+                },
+                //databaseResponseGet
+                null,
+                //databaseResponseUpdate
+                null
+            };
+            //Happy Path - Leave Queue
+            yield return new object[]
+            {
+                new SlashRequest
+                {
+                    Channel_Id = "123",
+                    Enterprise_Id = "123",
+                    User_Id = "123"
+                },
+                new SlackResponseDto
+                {
+                    text = $"@123 has left the queue" + "\n" +
+                           $"@321 ITS GO TIME",
+                    response_type = BasicResponseTypes.in_channel
+                },
+                //databaseResponseGet
+                new QueueDto
+                {
+                    ChannelId = "123",
+                    Queue = new Queue<string>(new List<string> {"123", "321"})
+                },
+                //databaseResponseUpdate
+                new QueueDto
+                {
+                    ChannelId = "123",
+                    Queue = new Queue<string>(new List<string> {"321"})
                 }
             };
         }
