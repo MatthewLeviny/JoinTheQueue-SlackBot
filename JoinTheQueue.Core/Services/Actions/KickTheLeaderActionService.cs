6df8074cbbcebbb90d1a1f51cc370b5f631e6063
Matthew Leviny <matthew.leviny@carsales.com.au>
2020-08-30 15:09:44 +1000

    kick the leader get

--- JoinTheQueue.Core/Services/Actions/KickTheLeaderActionService.cs
+++ JoinTheQueue.Core/Services/Actions/KickTheLeaderActionService.cs
@@ -1,14 +1,40 @@
 ﻿using System.Threading.Tasks;
 using JoinTheQueue.Core.Dto;
+using JoinTheQueue.Core.Enum;
+using JoinTheQueue.Core.Repository;
 
 namespace JoinTheQueue.Core.Services.Actions
 {
     public class KickTheLeaderActionService :IActionService
     {
-        public string Key => "KickAction";
-        public Task PerformAction(Root request)
+        private readonly IQueueDatabase _queueDatabase;
+        private readonly IWebHookService _hookService;
+
+        public KickTheLeaderActionService(IQueueDatabase queueDatabase, IWebHookService hookService)
         {
-            throw new System.NotImplementedException();
+            _queueDatabase = queueDatabase;
+            _hookService = hookService;
+        }
+
+        public string Key => "KickAction";
+        public async Task PerformAction(Root request)
+        {
+            var queue = await _queueDatabase.GetQueue(request.channel.id, null);
+            if (queue == null)
+            {
+                var responseNoQueue = new SlackResponseDto()
+                {
+                    Text = "Queue Does not exist please create one",
+                    ResponseType = BasicResponseTypes.in_channel,
+                    DeleteOriginal = true,
+                };
+
+                await _hookService.TriggerWebHook(request.response_url, responseNoQueue);
+                return;
+            }
+            //empty queue
+
+            //Removed from queue
         }
     }
 }
